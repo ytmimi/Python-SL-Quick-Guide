5c31c89d3526e3ecbca914fc0878aa2779fe16fc
Yacin Tmimi <yacintmimi@gmail.com>
2018-10-21 23:43:18 -0400

    improved readability

--- graphql_client/src/components/FunctionCell/FunctionCell.js
+++ graphql_client/src/components/FunctionCell/FunctionCell.js
@@ -3,8 +3,10 @@ import './FunctionCell.css'
 
 const FunctionCell = (props) =>{
   return (
-    <div className={'Cell'} onClick={()=>props.click(props.name)}>
-      <p>{props.name+'()'}</p>
+    <div
+      className={'Cell'}
+      onClick={() => props.click(props.name)}>
+      <p>{`${props.name}()`}</p>
     </div>
   );
 }
